12db09fea4dbd95f988be7923bcedaed8446e23f
Vel2025 <velbalaiya@gmail.com>
2025-03-29 22:46:15 -0600

    for submission

---
+++ script.js
@@ -0,0 +1,7 @@
+//Part-3
+
+const registerForm = document.getElementById('register-form');
+const loginForm = document.getElementById('login-form');
+const errorDisplay = document.getElementById('errorDisplay');
+
+
